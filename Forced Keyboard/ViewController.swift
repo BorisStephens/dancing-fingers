7b084afe5ef5c38b12000f62d272200fc74a9ad4
Luke Stephens <lukestephens@lukes-macbook-pro.local>
2017-06-26 01:24:14 +1000

    Commit before transferring to a pan gesture subclass as touch press & moves are losing number of touches detected

--- Forced Keyboard/ViewController.swift
+++ Forced Keyboard/ViewController.swift
@@ -17,19 +17,21 @@ class ViewController: UIViewController {
         self.view.isMultipleTouchEnabled = true
         print(self.view.isMultipleTouchEnabled)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let keyboardHands = ForcedKeyboardGestureRecognizer(target: self, action: #selector(reset), threshold: 0.5)
+        self.view.addGestureRecognizer(keyboardHands)
+        
     }
     
-    func reset(){
-        Stroke1 = false
-        Stroke2 = false
-        Stroke3 = false
-        Stroke4 = false
+    @objc func reset(){
         
         self.drawCircle(x: 20, color: UIColor.red.cgColor,    stroke: false)
         self.drawCircle(x: 140, color: UIColor.green.cgColor,  stroke: false)
         self.drawCircle(x: 260, color: UIColor.blue.cgColor,   stroke: false)
         self.drawCircle(x: 380, color: UIColor.orange.cgColor, stroke: false)
     }
+    
+    
     
     @IBOutlet weak var labelLetter: UILabel!
     @IBOutlet weak var labelSentance: UITextView!
@@ -39,153 +41,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    var Stroke1 = false,
-        Stroke2 = false,
-        Stroke3 = false,
-        Stroke4 = false
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        let threashold:CGFloat = 1.0
-        for(index, touch) in touches.enumerated() {
-            
-            if #available(iOS 9.0, *) {
-                if traitCollection.forceTouchCapability == UIForceTouchCapability.available {
-                    
-                    // 3D Touch capable
-                    let force = touch.force///touch.maximumPossibleForce
-                    
-                    // First
-                    if force > threashold && index == 0 {
-                        Stroke1 = true
-                    }
-                    if force < threashold && index == 0 {
-                        Stroke1 = false
-                    }
-                    
-                    // Second
-                    if force > threashold && index == 1 {
-                        Stroke2 = true
-                    }
-                    if force < threashold && index == 1 {
-                        Stroke2 = false
-                    }
-                    
-                    // Third
-                    if force > threashold && index == 2 {
-                        Stroke3 = true
-                    }
-                    if force < threashold && index == 2 {
-                        Stroke3 = false
-                    }
-                    
-                    // Fourth
-                    if force > threashold && index == 3 {
-                        Stroke4 = true
-                    }
-                    if force < threashold && index == 2 {
-                        Stroke4 = false
-                    }
-                }
-            }
-            
-            self.drawCircle(x: 20, color: UIColor.red.cgColor,    stroke: Stroke1)
-            self.drawCircle(x: 40, color: UIColor.green.cgColor,  stroke: Stroke2)
-            self.drawCircle(x: 60, color: UIColor.blue.cgColor,   stroke: Stroke3)
-            self.drawCircle(x: 80, color: UIColor.orange.cgColor, stroke: Stroke4)
-        }
-        
-        
-        self.detectNumber()
-        
-    }
-    
-    func detectLetter(){
-        var String = ""
-        if(Stroke1){
-            String += "1"
-        } else {
-            String += "0"
-        }
-        
-        if(Stroke2){
-            String += "1"
-        } else {
-            String += "0"
-        }
-        
-        if(Stroke3){
-            String += "1"
-        } else {
-            String += "0"
-        }
-        
-        if(Stroke4){
-            String += "1"
-        } else {
-            String += "0"
-        }
-        
-        /* Alphabet */
-        self.labelLetter.text = "\(String)"
-        
-        if(self.labelLetter.text == "1000"){
-           self.labelLetter.text = "A"
-        }
-        if(self.labelLetter.text == "1100"){
-            self.labelLetter.text = "B"
-        }
-        if(self.labelLetter.text == "1110"){
-            self.labelLetter.text = "C"
-        }
-        if(self.labelLetter.text == "1111"){
-            self.labelLetter.text = "D"
-        }
-    }
-    
-    func detectNumber(){
-        
-        var String = 0
-        if(Stroke1){
-            String += 1
-        }
-        
-        if(Stroke2){
-            String += 2
-        }
-        
-        if(Stroke3){
-            String += 4
-        }
-        
-        if(Stroke4){
-            String += 8
-        }
-        
-        /* Numbers */
-        self.labelLetter.text = "\(String)"
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //print("Count is up to \(touches.count)")
-        
-        for obj in touches {
-            let touch = obj as UITouch
-            let location = touch.location(in: self.view)
-            //print(location)
-        }
-    }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.reset()
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("A cancel event happened")
-    }
-    
-    
-    
     
     /* DRAWING */
     func drawCircle(x:Int,color: CGColor, stroke: Bool){
@@ -213,3 +71,108 @@ class ViewController: UIViewController {
     
 }
 
+import UIKit.UIGestureRecognizerSubclass
+
+// MARK: GestureRecognizer
+class ForcedKeyboardGestureRecognizer: UIGestureRecognizer
+{
+    var vibrateOnDeepPress = false
+    let threshold: CGFloat
+    
+    private var deepPressed: Bool = false
+    private var Stroke1 = false
+    private var Stroke2 = false
+    private var Stroke3 = false
+    private var Stroke4 = false
+    
+    required init(target: AnyObject?, action: Selector, threshold: CGFloat)
+    {
+        self.threshold = threshold
+        super.init(target: target, action: action)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        do {
+            print("Forced Key Touches Begun")
+            handleTouch(touches:touches)
+        }
+    }
+    
+    override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
+        print("Forced Key Estimate Attempt")
+        handleTouch(touches: touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        do {
+            print("Forced Key Supposed Movement")
+            handleTouch(touches:touches)
+        }
+    }
+    
+    override func pressesChanged(_ presses: Set<UIPress>, with event: UIPressesEvent) {
+        print("Forced Key Supposed Movement")
+        handlePress(touches: presses)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesEnded(touches, with: event)
+        
+        state = deepPressed ? UIGestureRecognizerState.ended : UIGestureRecognizerState.failed
+        
+        deepPressed = false
+    }
+    
+    private func handleTouch(touches: Set<UITouch>){
+    }
+    
+    private func handlePress(touches: Set<UIPress>){
+        state = UIGestureRecognizerState.began
+        deepPressed = true
+        
+        // MARK: Exclusions, Reset Fingers Not There
+        switch touches.count {
+        case 1:
+            Stroke2 = false
+            Stroke3 = false
+            Stroke4 = false
+        case 2:
+            Stroke3 = false
+            Stroke4 = false
+        case 3:
+            Stroke4 = false
+        default: break
+        }
+        
+        // MARK: Fingers, Check Them All
+        let threashold = self.threshold
+        for(index, touch) in touches.enumerated() {
+            let force = touch.force
+            
+            // First
+            if force > threashold && index == 0 {Stroke1 = true}
+            if force < threashold && index == 0 {Stroke1 = false}
+            
+            // Second
+            if force > threashold && index == 1 {Stroke2 = true}
+            if force < threashold && index == 1 {Stroke2 = false}
+            
+            // Third
+            if force > threashold && index == 2 {Stroke3 = true}
+            if force < threashold && index == 2 {Stroke3 = false}
+            
+            // Fourth
+            if force > threashold && index == 3 {Stroke4 = true}
+            if force < threashold && index == 3 {Stroke4 = false}
+        }
+        
+        // MARK: Binary, Return
+        var String = ""
+        if(Stroke1){String += "1"} else {String += "0"}
+        if(Stroke2){String += "1"} else {String += "0"}
+        if(Stroke3){String += "1"} else {String += "0"}
+        if(Stroke4){String += "1"} else {String += "0"}
+        print("\(String) Touches: \(touches.count)")
+        return
+    }
+}
